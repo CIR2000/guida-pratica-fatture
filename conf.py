fd20bafa9caffec45eea0147443824ed8b52a8a5
Nicola Iarocci <nicola@nicolaiarocci.com>
2014-04-03 16:12:36 +0200

    ISBN for mobi

--- conf.py
+++ conf.py
@@ -291,6 +291,8 @@ epub_scheme = 'ISBN'
 # The unique identifier of the text. This can be a ISBN number
 # or the project homepage.
 epub_identifier = '9788898895007'
+# for mobi
+#epub_identifier = '9788898895014'
 
 # A unique identification for the text.
 #epub_uid = ''
